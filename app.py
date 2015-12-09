9e7c3388212d5d52e9192f9ac6332e49feea1fcc
Libert R Schmidt <katari@waynasolutions.com>
2015-12-09 15:35:34 -0500

    request

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for,redirect
 
 app = Flask(__name__)
 
@@ -6,9 +6,14 @@ app = Flask(__name__)
 def index():
     return 'R2D2'
 
-@app.route('/login')
+@app.route('/login',methods=['POST', 'GET'])
 def login():
-    return 'Please Log In'
+    if request.method == "POST":
+        if request.form['username'] == 'admin' and request.form['password'] == 'admin':
+            return render_template(('base.html'))
+        else:
+            return "Inconrrect user and password"
+    return render_template('login.html')
 
 @app.route('/user/<name>')
 def user(name):
