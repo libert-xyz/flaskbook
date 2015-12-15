6e9c94cebe8fca7ce6f4bac4b8eb94fc50943125
Libert R Schmidt <katari@waynasolutions.com>
2015-12-14 19:24:37 -0500

    register|wtforms

--- crud/views.py
+++ crud/views.py
@@ -1,6 +1,7 @@
-from flask_crud import app
+from flask_crud import app, db
 from flask import render_template, request, url_for,redirect,flash,session
 from models import User, bcrypt
+from form import RegistrationForm,LoginForm
 import logging
 from logging.handlers import RotatingFileHandler
 
@@ -14,9 +15,12 @@ def index():
 
 @app.route('/login',methods=['POST', 'GET'])
 def login():
-    error = None
+    loginError = None
+    login = LoginForm()
 
-    if request.method == "POST":
+    ##LOGIN
+
+    if login.validate_on_submit():
         users = User.query.filter_by(username=request.form['username']).first()
         if users is not None and bcrypt.check_password_hash(
         users.password,request.form['password']):
@@ -24,10 +28,33 @@ def login():
             session['username'] = request.form['username']
             return redirect(url_for('index'))
         else:
-            error = "Incorrect User and Password"
+            loginError = "Incorrect User and Password"
             app.logger.warning('Incorrect passwword for username (%s)',
                                 request.form.get('username'))
-    return render_template('login.html',error=error)
+    return render_template('login.html',form1=RegistrationForm(),form2=login,loginError=loginError,registerError=None)
+
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+
+    register = RegistrationForm()
+    registerError = None
+    if register.validate_on_submit():
+        user_unique = User.query.filter_by(username=request.form['username']).first()
+        email_unique = User.query.filter_by(email=request.form['email']).first()
+        if user_unique is None and email_unique is None:
+
+            user = User(register.fullname.data, register.username.data, register.email.data,
+            register.password.data)
+            db.session.add(user)
+            db.session.commit()
+            session['username'] = request.form['username']
+            flash("Thank's for registering")
+            return redirect(url_for('index'))
+        else:
+            registerError = "Email or Username already taken"
+    return render_template('login.html',form1=register,form2=LoginForm(),loginError=None,registerError=registerError)
+
 
 @app.route('/user/<name>')
 def user(name):
